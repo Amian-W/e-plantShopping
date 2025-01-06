871436f4e8b9cec06284680191446e31523b75df
Naima <naima.wahbi92@gmail.com>
2025-01-05 22:48:54 -0500

    Finished ProductList component Layout

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,21 +1,22 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 export const CartSlice = createSlice({
-  name: 'cart',
-  initialState: {
-    items: [], // Initialize items as an empty array
-  },
-  reducers: {
-    addItem: (state, action) => {
-    
+    name: 'cart',
+    initialState: {
+        items: [], // Initialize items as an empty array
     },
-    removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
+    reducers: {
+        addItem: (state, action) => {
+            const newItem = action.payload; // Get the plant details from the payload
+            state.items.push(newItem); // Add the plant to the items array     
+        },
+        removeItem: (state, action) => {
+        },
+        updateQuantity: (state, action) => {
 
-    
+
+        },
     },
-  },
 });
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
